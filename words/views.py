c4b34b8aac71523a58501d7a29dffaff67adff4d
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-05-12 15:12:14 +0200

    Add session counter and reset game logic

--- words/views.py
+++ words/views.py
@@ -5,25 +5,39 @@ from words.models import Word
 def index(request):
 
     result = ""
+    num_attempts = request.session.get("num_attempts", 0)
 
     if request.method == "POST":
 
-        word_id = request.POST.get("word_id")
-        word = Word.objects.get(id=word_id)
+        if "end_session" in request.POST:
 
-        answer = request.POST.get("answer")
+            if "num_attempts" in request.session:
+                del request.session["num_attempts"]
 
-        if word.text_en.lower() == answer.lower():
-            result = "Dobrze"
+            num_attempts = 0
+            word = Word.objects.order_by("?").first()
+
         else:
-            result = f"Błąd. Poprawna odpowiedź: {word.text_en}"
 
-        word_new = Word.objects.order_by("?").first()
+            num_attempts += 1
+            request.session["num_attempts"] = num_attempts
 
-        while word_new.id == word.id and Word.objects.count() > 1:
+            word_id = request.POST.get("word_id")
+            word = Word.objects.get(id=word_id)
+
+            answer = request.POST.get("answer")
+
+            if word.text_en.lower() == answer.lower():
+                result = "Dobrze"
+            else:
+                result = f"Błąd. Poprawna odpowiedź: {word.text_en}"
+
             word_new = Word.objects.order_by("?").first()
 
-        word = word_new
+            while word_new.id == word.id and Word.objects.count() > 1:
+                word_new = Word.objects.order_by("?").first()
+
+            word = word_new
 
     else:
         word = Word.objects.order_by("?").first()
@@ -33,6 +47,7 @@ def index(request):
         "words/index.html",
         {
             "word": word,
-            "result": result
+            "result": result,
+            'num_attempts': num_attempts
         }
     )
