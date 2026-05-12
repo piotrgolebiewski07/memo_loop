a2b128f67721d519dfff69070d88a36167ce002e
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-05-12 11:29:57 +0200

    Add word checking and random next word logic

--- words/views.py
+++ words/views.py
@@ -1,14 +1,33 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from words.models import Word
 
 
 def index(request):
-    word = Word.objects.order_by("?")[0]
+
+    result = ""
+
+    if request.method == "POST":
+
+        word_id = request.POST.get("word_id")
+        word = Word.objects.get(id=word_id)
+
+        answer = request.POST.get("answer")
+
+        if word.text_en.lower() == answer.lower():
+            result = "Dobrze"
+        else:
+            result = "Błąd"
+
+    else:
+        word = Word.objects.order_by("?").first()
+
+    word = Word.objects.order_by("?").first()
 
     return render(
         request,
         "words/index.html",
-        {"word": word}
+        {
+            "word": word,
+            "result": result
+        }
     )
-
