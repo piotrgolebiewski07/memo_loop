e446c9dc7dc5bd488beda1f0264c8d45a6cb7f4f
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-07-17 09:28:25 +0200

    refactor: split views into modules

---
+++ words/views/auth.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/")
+    else:
+        form = UserCreationForm()
+
+    return render(
+        request,
+        "registration/register.html",
+        {
+            "form": form,
+        }
+    )
+
