c9e73aa530298471241f2171d4cfeeb47260332c
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-07-18 13:01:26 +0200

    Add model tests

--- words/tests/test_models.py
+++ words/tests/test_models.py
@@ -1,4 +1,5 @@
-from words.models import WordSet
+from words.models import WordSet, Word, StudySession
+from django.contrib.auth.models import User
 
 import pytest
 
@@ -56,4 +57,127 @@ def test_word_set_can_be_saved_to_database():
 
     saved_word_set = WordSet.objects.get()
     assert saved_word_set.name == "Angielski A1"
+
+
+@pytest.mark.django_db
+def test_word_set_owner():
+    user = User.objects.create_user(
+        username="jan",
+        password="haslo",
+    )
+
+    WordSet.objects.create(
+        name="Angielski A1",
+        description="Podstawowe słówka",
+        level="A1",
+        image="default.jpg",
+        slug="angielski-a1",
+        owner=user
+    )
+
+    saved_word_set = WordSet.objects.get()
+    assert saved_word_set.owner.username == "jan"
+
+
+def test_word_str():
+    word = Word(
+        text_pl="drzewo",
+        text_en="tree",
+    )
+
+    assert str(word) == "drzewo - tree"
+
+
+@pytest.mark.django_db
+def test_word_word_set():
+    word_set = WordSet.objects.create(
+        name="Angielski A1",
+        description="Podstawowe słówka",
+        level="A1",
+        image="default.jpg",
+        slug="angielski-a1",
+    )
+
+    Word.objects.create(
+        text_pl="drzewo",
+        text_en="tree",
+        word_set=word_set,
+    )
+
+    save_word = Word.objects.get()
+    assert save_word.word_set.name == "Angielski A1"
+
+
+@pytest.mark.django_db
+def test_word_set_word():
+    word_set = WordSet.objects.create(
+        name="Angielski A1",
+        description="Podstawowe słówka",
+        level="A1",
+        image="default.jpg",
+        slug="angielski-a1",
+    )
+
+    Word.objects.create(
+        text_pl="drzewo",
+        text_en="tree",
+        word_set=word_set,
+    )
+
+    Word.objects.create(
+        text_pl="dom",
+        text_en="house",
+        word_set=word_set,
+    )
+
+    assert word_set.words.count() == 2
+
+
+@pytest.mark.django_db
+def test_study_session_str():
+    word_set = WordSet.objects.create(
+        name="Angielski A1",
+        description="Podstawowe słówka",
+        level="A1",
+        image="default.jpg",
+        slug="angielski-a1",
+    )
+
+    user = User.objects.create_user(
+        username="jan",
+        password="haslo",
+    )
+
+    study_session = StudySession.objects.create(
+        user=user,
+        word_set=word_set,
+        success_rate=100
+    )
+
+    assert str(study_session) == f"{user.username} - {word_set.name} - 100%"
+
+
+@pytest.mark.django_db
+def test_study_session_default_values():
+    user = User.objects.create_user(
+        username="jan",
+        password="haslo",
+    )
+
+    word_set = WordSet.objects.create(
+        name="Angielski A1",
+        description="Podstawowe słówka",
+        level="A1",
+        image="default.jpg",
+        slug="angielski-a1",
+    )
+
+    study_session = StudySession.objects.create(
+        user=user,
+        word_set=word_set
+    )
+
+    assert study_session.correct_answers == 0
+    assert study_session.wrong_answers == 0
+    assert study_session.success_rate == 0
 
