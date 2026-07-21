d58c136cf3e53bbf482b0fbb5c4c0bbc725dfb3c
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-07-21 11:15:44 +0200

    Extend ready sets view tests

--- words/tests/test_views.py
+++ words/tests/test_views.py
@@ -76,3 +76,67 @@ def test_ready_sets_page_does_not_display_deleted_set(client):
 
     response = client.get("/ready-sets/")
     assertNotContains(response, "Angielski A1")
+
+
+@pytest.mark.django_db
+def test_ready_sets_page_displays_non_featured_set_when_show_all_is_true(client):
+    WordSet.objects.create(
+        name="Angielski A1",
+        description="Podstawowe słówka",
+        level="A1",
+        image="default.jpg",
+        slug="angielski-a1",
+        is_public=True,
+        is_featured=False,
+        is_deleted=False,
+    )
+
+    response = client.get("/ready-sets/?show-all=true")
+    assertContains(response, "Angielski A1")
+
+
+@pytest.mark.django_db
+def test_ready_sets_page_does_not_display_private_set(client):
+    WordSet.objects.create(
+        name="Angielski C1",
+        description="Zaawansowane słówka",
+        level="C1",
+        image="default.jpg",
+        slug="angielski-c1",
+        is_public=False,
+        is_featured=True,
+        is_deleted=False,
+    )
+
+    response = client.get("/ready-sets/?show-all=true")
+    assert response.status_code == 200
+    assertNotContains(response, "Angielski C1")
+
+
+@pytest.mark.django_db
+def test_ready_sets_page_context_contains_public_sets_count(client):
+    WordSet.objects.create(
+        name="Angielski A1",
+        description="Podstawowe słówka",
+        level="A1",
+        image="default.jpg",
+        slug="angielski-a1",
+        is_public=True,
+        is_featured=False,
+        is_deleted=False,
+    )
+
+    WordSet.objects.create(
+        name="Angielski C1",
+        description="Zaawansowane słówka",
+        level="C1",
+        image="default.jpg",
+        slug="angielski-c1",
+        is_public=True,
+        is_featured=True,
+        is_deleted=False,
+    )
+
+    response = client.get("/ready-sets/")
+    assert response.context["set_count"] == 2
+
